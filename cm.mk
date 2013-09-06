8c932f10ca5722bd0eb073f6a90787c974b0ec17
Dave Carey <carvsdriver@gmail.com>
2013-09-06 07:46:15 -0700

    device:exhilarate: update build fingerprint
     - Update build fingerprint to 4.3
     - Remove legacy bootanimation setting
    
    Change-Id: I6c9e117233ff8f71f540ff9e9ecdc51b651674b0

--- cm.mk
+++ cm.mk
@@ -9,9 +9,7 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGH-I577 TARGET_DEVICE=SGH-I577 BUILD_FINGERPRINT="samsung/SGH-I577/SGH-I577:4.1.2/JZO54K/UCLH3:user/release-keys" PRIVATE_BUILD_DESC="SGH-I577-user 4.1.2 JZO54K UCLH3 release-keys"
-
-TARGET_BOOTANIMATION_NAME := 480
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGH-I577 TARGET_DEVICE=SGH-I577 BUILD_FINGERPRINT="samsung/SGH-I577/SGH-I577:4.3/JZO54K/UCMC1:user/release-keys" PRIVATE_BUILD_DESC="SGH-I577-user 4.3 JZO54K UCMC1 release-keys"
 
 PRODUCT_NAME := cm_exhilarate
 PRODUCT_DEVICE := exhilarate
