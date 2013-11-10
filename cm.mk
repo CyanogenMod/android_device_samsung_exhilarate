f762e9a102ff4d31b2f7bfa84f2392ce0030bbfd
Dave Carey <carvsdriver@gmail.com>
2013-11-10 07:45:28 -0800

    exhilarate: update build fingerprint to KRT16M
    Change-Id: Ia9b7382bc9f544fdf9c575e73dc753be6161a0ca

--- cm.mk
+++ cm.mk
@@ -9,7 +9,7 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGH-I577 TARGET_DEVICE=SGH-I577 BUILD_FINGERPRINT="samsung/SGH-I577/SGH-I577:4.4/JZO54K/UCLH3:user/release-keys" PRIVATE_BUILD_DESC="SGH-I577-user 4.4 JZO54K UCLH3 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGH-I577 TARGET_DEVICE=SGH-I577 BUILD_FINGERPRINT="samsung/SGH-I577/SGH-I577:4.4/KRT16M/UCLH3:user/release-keys" PRIVATE_BUILD_DESC="SGH-I577-user 4.4 KRT16M UCLH3 release-keys"
 
 PRODUCT_NAME := cm_exhilarate
 PRODUCT_DEVICE := exhilarate
